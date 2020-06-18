e685e9c9216ff9167913fcd7d5c72171d7dabf59
jazmyn cortez <jazmyncortez@gmail.com>
2020-06-18 15:12:15 -0700

    take over

--- db/seeds.rb
+++ db/seeds.rb
@@ -8,13 +8,11 @@
 
 dogs = [
     {
-        id:1,
         name:"Fiddlesticks",
         age: 4,
         enjoys: "chasing my tail"
     },
     {
-        id:2,
         name:"Beevis",
         age: 5,
         enjoys: "chewing my toy"
